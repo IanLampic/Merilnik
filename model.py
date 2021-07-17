7b5fd52910ee2f402e1898b587163c2d56570984
Ian Lampič <lampic.ian@gmail.com>
2021-07-17 19:38:08 +0200

    test1

--- model.py
+++ model.py
@@ -1,57 +1,55 @@
 osnovna_poraba_kisika = 3.5 
+from datetime import datetime
 
-class Merilnik_teka:
+class MerilnikTeka:
    
     def __init__(self, dolzina, cas, nacin, strmina, teza):
         self.dolzina = dolzina
         self.cas = cas
         self.nacin = nacin
+        #Pri načinu tek predstavlja resnico, hoja pa laž
         self.strmina = strmina
         self.teza = teza
-    
-    osnovna_poraba_kisika = 3,5
-    #za strmino nastavijo pri tekstovnemu vmesniku:
-    #-brez = 0 %
-    #-majhen klanec = 5 %
-    #-srednje velik klanec = 10 %
-    #-velik klanec = 15 %
-    #-zelo velik klanec = 20 %
 
-    #enota za porabo kisika ml / kg min
+    
+
+    osnovna_poraba_kisika = 3,5
+    """za strmino nastavijo pri tekstovnemu vmesniku:"""
+    """-brez = 0 %"""
+    """-majhen klanec = 5 %"""
+    """-srednje velik klanec = 10 %"""
+    """-velik klanec = 15 %"""
+    """-zelo velik klanec = 20 %"""
     
     def hitrost(self):
         return self.dolzina / self.cas
 
     def poraba_kisika_horizontalno(self):
-        #nacin predstavlja obliko gibanja (tek ali hoja)#
-        if self.nacin == 'hoja':
+        """nacin predstavlja obliko gibanja (tek ali hoja)"""
+        if self.nacin:
             return 0.1
-        elif self.nacin == 'tek':
-            return 0.2
         else:
-            return 0
-    
+            return 0.2
+        """enota za porabo kisika ml / kg min"""
+
     def poraba_kisika_vertikalno(self):
         return 1.8 
  
     def odvisnost_vertikalnega_gibanja_od_strmine(self):
-        return Merilnik_teka.poraba_kisika_vertikalno(self) * (self.strmina * 0.01)
+        return self.poraba_kisika_vertikalno() * (self.strmina * 0.01)
 
     def poraba_kisika(self):
-        if self.nacin == 'hoja':
-            return Merilnik_teka.hitrost(self) * Merilnik_teka.poraba_kisika_horizontalno(self) + Merilnik_teka.hitrost(self) * Merilnik_teka.odvisnost_vertikalnega_gibanja_od_strmine(self) + osnovna_poraba_kisika
-        elif self.nacin == 'tek':
-            return Merilnik_teka.hitrost(self) * Merilnik_teka.poraba_kisika_horizontalno(self) + Merilnik_teka.hitrost(self) * Merilnik_teka.odvisnost_vertikalnega_gibanja_od_strmine(self) + osnovna_poraba_kisika
+        if self.nacin:
+            return self.hitrost() * self.poraba_kisika_horizontalno() + self.hitrost() * self.odvisnost_vertikalnega_gibanja_od_strmine() + osnovna_poraba_kisika
         else:
-            return 0
+            return self.hitrost() * self.poraba_kisika_horizontalno() + self.hitrost() * self.odvisnost_vertikalnega_gibanja_od_strmine() + osnovna_poraba_kisika
 
     def poraba_kisika_s_tezo(self):
-        return '%.2f'%(Merilnik_teka.poraba_kisika(self) * self.teza * self.cas) + ' ml/(kg min)'
+        return str(round(self.poraba_kisika() * self.teza * self.cas, 2)) + ' ml/(kg min)'
 
-    
     def poraba_kalorij(self):
-        return '%.2f'%(Merilnik_teka.poraba_kisika_s_tezo(self) * 5) + ' kcal'
-
+        return str(round(float(self.poraba_kisika_s_tezo()[:-12]) * 5, 2)) + ' kcal'
+        
 
 class Dnevnik_gibanja:
     def __init__(self, dolzina, cas, nacin, strmina, teza, datum):
@@ -61,14 +59,51 @@ class Dnevnik_gibanja:
         self.strmina = strmina
         self.teza = teza
         self.datum = datum
-        self.seznam_gibanj = []
+        self.slovar_gibanj = {key:set() for key in range(1, 13)}
+
+
+    @staticmethod 
+    def vrni_dan(danasnji_datum):
+        danasnji_datum = datetime.now()
+        return danasnji_datum.strftime("%d")
+    
+    @staticmethod 
+    def vrni_mesec(danasnji_datum):
+        danasnji_datum = datetime.now()
+        return danasnji_datum.strftime("%m")
+    
+    @staticmethod 
+    def vrni_leto(danasnji_datum):
+        danasnji_datum = datetime.now()
+        return danasnji_datum.strftime("%Y")
+
 
     def nov_vnos(self):
-        return self.seznam_gibanj.append((self.dolzina, self.cas, self.nacin, self.strmina, self.teza, self.datum))
+        danasnji_datum = datetime.now() 
+        for i in range(1, 13):
+            if danasnji_datum.strftime("%m") == i:
+                self.slovar_gibanj['i'].add(danasnji_datum)
+            else:
+                continue
+        return self.slovar_gibanj
 
+        
+    #kako delujejo datumi, kako iz dneva, meseca, leta naredis objekt datum, strftime, kako objekta datum dobis mesec,
     def izbris(self):
-        return self.seznam_gibanj.pop()
+        danasnji_datum = datetime.now() 
+        for i in range(1, 13):
+            if danasnji_datum.strftime("%m") == i:
+                self.slovar_gibanj['i'].pop()
+            else:
+                continue
+        return self.slovar_gibanj
 
+    def razdelitev(self):
+        #strftime
+        d = datetime.datetime.now()
+        m = d.strftime('%m')
+    
+    #funkcija, ki iz slovarja gibanj, dobi seznam vseh gibanj, nardis unijo vseh values iz kljucov
     def razdelitev_po_mesecih(self):
         januar = []
         februar = []
@@ -83,6 +118,7 @@ class Dnevnik_gibanja:
         november = []
         december = []
         datumi = []
+
         for podatki in self.seznam_gibanj:
             datumi.append(podatki[-1])
             for element in datumi:
@@ -117,5 +153,26 @@ class Dnevnik_gibanja:
 
 
 
-danes1 = Merilnik_teka(4, 5, 'hoja', 5, 5)
-danes2 = Dnevnik_gibanja(4, 5, 'hoja', 5, 5, '3. 4. 2021')
+danes1 = MerilnikTeka(4, 5, True, 5, 5)
+danasnji_datum = datetime.now()
+danes2 = Dnevnik_gibanja(4, 5, True, 5, 5, danasnji_datum)
+
+#kako iz stringa dobis datum
+#from datetime import datetime
+#
+#timestamp = 1528797322
+#date_time = datetime.fromtimestamp(timestamp)
+#
+#print("Date time object:", date_time)
+#
+#d = date_time.strftime("%m/%d/%Y, %H:%M:%S")
+#print("Output 2:", d)	
+#
+#d = date_time.strftime("%d %b, %Y")
+#print("Output 3:", d)
+#
+#d = date_time.strftime("%d %B, %Y")
+#print("Output 4:", d)
+#
+#d = date_time.strftime("%I%p")
+#print("Output 5:", d)
--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,4 +1,6 @@
-from model import Merilnik_teka
+# from _typeshed import Self
+
+from model import MerilnikTeka
 from datetime import date
 
 razdalja = input('Koliko ste pretekli (v kilometrih)? ')
@@ -7,5 +9,5 @@ način_gibanja = input('Kako ste se gibali (hoja ali tek)? ')
 strmina = input('Kakšen je bil klanec (izbirate lahko med: zelo velik, velik, srednje velik, majhen, ni bilo klanca)? ')
 teža = input('Kakšna je vaša telesna teža (v kilogramih)? ')
 
-print('Porabili ste ' + str(Merilnik_teka.poraba_kisika_s_tezo()) + ' kilometrov')
+print('Porabili ste ' + str(self.poraba_kisika_s_tezo()) + ' kilometrov')
 print('Tekli ste ' + str(čas) + ' ur')
